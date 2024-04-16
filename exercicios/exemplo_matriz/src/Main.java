9c0d880ffae9af131152bc149d674ad60ba2ead1
kleber <44203711+klebber1@users.noreply.github.com>
2024-04-16 18:39:52 -0300

    Add files via upload

---
+++ exercicios/exemplo_matriz/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int [][]matriz = new int[3][3];
+        for (int i = 0; i <3; i++) {
+            for (int j = 0; j < 3; j++) {
+
+            }
+        }
+    }
+}
