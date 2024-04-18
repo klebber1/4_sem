812fe30e80d97185fd1e5097612b1bd1a976875e
kleber <44203711+klebber1@users.noreply.github.com>
2024-04-18 10:41:12 -0300

    Add files via upload

---
+++ exercicios/exemplo_vetor/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] vetor = new int[10];
+        /*para inicializar o vetro com dados usar:      int[]vetor= {0,1,2,3,4,5,6,7,8,9};  */
+        for (int i = 0; i < 10; i++) {
+            System.out.println(vetor[i]);
+
+            /*
+            * for(int x : vetor){
+            *  System.out.println(x)};
+            * */
+        }
+    }
+}
