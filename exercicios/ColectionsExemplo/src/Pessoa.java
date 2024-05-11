7065e132dde7b5c41ae0b88c757c73d4c659a0ba
kleber <44203711+klebber1@users.noreply.github.com>
2024-05-11 00:51:50 -0300

    Add files via upload

---
+++ exercicios/ColectionsExemplo/src/Pessoa.java
@@ -0,0 +1,50 @@
+public class Pessoa implements Comparable<Pessoa>{
+    private int id;
+    private String nome;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Pessoa(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa: " + id + "\n" +
+                "Nome: " + nome + "\n";
+    }
+
+    @Override
+    public int hashCode() {
+        //retornar numero de letras que há em nome
+        return nome.length();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return ((o instanceof Pessoa) && ((Pessoa)o).id == this.id);
+    }
+
+    @Override
+    public int compareTo(Pessoa o) {
+        if (this.id < o.id)return -1;
+        if (this.id > o.id)return 1;
+        return 0;
+        //return Integer.compare(this.id, o.id);
+    }
+}
